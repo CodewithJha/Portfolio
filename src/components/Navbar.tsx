5183dff7a5c5d1ac198def389c99a74bd6d6b247
Priyanshu Jha <155089480+CodewithJha@users.noreply.github.com>
2026-02-27 17:42:34 +0530

    Change navbar title from 'Codewithjha' to 'CODEWITHJHA'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Codewithjha
+          CODEWITHJHA
         </a>
         <a
           href="mailto:Priyanshujha254@gmail.com"
